2898941fef457e17debaf8cccefd3c4560c2dc4a
Ragunaathan Pandurangan <ragunaathan.pandurangan@syncfusion.com>
2020-02-18 22:54:04 +0530

    Updated the sample using Transaction with rollback once DbUpdateConcurrencyException occurred

--- EFCoreTransactionSample/TransactionSampleWebAPI/Controllers/SQLServerController.cs
+++ EFCoreTransactionSample/TransactionSampleWebAPI/Controllers/SQLServerController.cs
@@ -140,5 +140,69 @@ namespace TransactionSampleWebAPI.Controllers
 
             return new JsonResult(new { result = projectId });
         }
+
+        [HttpPost("transactionrollback/{brandId}")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
+        public async Task<ActionResult> AddProjectsUsingTransactionsWithRollback(int brandId)
+        {
+            int retryCount = 0;
+            int projectId = 0;
+
+            while (retryCount < 3)
+            {
+                using (var transaction = await context.Database.BeginTransactionAsync().ConfigureAwait(false))
+                {
+                    var projectDetails = new TrProjectDetails()
+                    {
+                        BrandId = brandId,
+                        IsActive = true
+                    };
+                    await context.TrProjectDetails.AddAsync(projectDetails).ConfigureAwait(false);
+
+                    try
+                    {
+                        var sequenceIdDetails = await context.TrProjectSequenceMapper.FirstOrDefaultAsync(i => i.IsActive && i.BrandId == brandId)
+                                                .ConfigureAwait(false);
+
+                        projectDetails.BrandProjectId = sequenceIdDetails.SequenceId;
+                        sequenceIdDetails.SequenceId++;
+
+                        if (retryCount == 0)
+                        {
+                            var sequenceId = new SqlParameter("sequenceId", sequenceIdDetails.SequenceId);
+                            var dbBrandId = new SqlParameter("brandId", brandId);
+                            context.Database.ExecuteSqlRaw("UPDATE TR_ProjectSequenceMapper SET SequenceId = @sequenceId WHERE BrandId = @brandId", dbBrandId, sequenceId);
+                        }
+
+                        await context.SaveChangesAsync().ConfigureAwait(false);
+
+                        var projectIdMapperDetails = new TrProjectMapper()
+                        {
+                            ProjectId = projectDetails.Id,
+                            IsActive = true
+                        };
+
+                        await context.TrProjectMapper.AddAsync(projectIdMapperDetails).ConfigureAwait(false);
+                        await context.SaveChangesAsync().ConfigureAwait(false);
+
+
+                        await transaction.CommitAsync().ConfigureAwait(false);
+
+                        projectId = projectDetails.BrandProjectId;
+                        retryCount = 3;
+                    }
+                    catch (DbUpdateConcurrencyException ex)
+                    {
+                        retryCount++;
+                        await transaction.RollbackAsync().ConfigureAwait(false);
+                        EntityEntry exceptionEntry = ex.Entries.Single();
+                        var databaseEntry = exceptionEntry.GetDatabaseValues();
+                        exceptionEntry.OriginalValues.SetValues(databaseEntry);
+                    }
+                }
+            }
+
+            return new JsonResult(new { result = projectId });
+        }
     }
 }
