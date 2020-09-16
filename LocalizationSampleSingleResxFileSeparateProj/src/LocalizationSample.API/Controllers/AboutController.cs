1959738e49fc868b463c940530b129fcb8381d8a
Ragunaathan Pandurangan <ragunaathan.pandurangan@syncfusion.com>
2020-09-16 18:30:43 +0530

    Committed the new sample where resource file referred from another project

---
+++ LocalizationSampleSingleResxFileSeparateProj/src/LocalizationSample.API/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using LocalizationSample.Resources;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationSample.API.Controllers
+{
+    [Route("{culture:culture}/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IStringLocalizer<SharedResource> localizer;
+        public AboutController(IStringLocalizer<SharedResource> localizer)
+        {
+            this.localizer = localizer;
+        }
+        public string Get()
+        {
+            return localizer["About"];
+        }
+    }
+}
---
+++ LocalizationSampleSingleResxFileSeparateProj/src/LocalizationSample.API/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using LocalizationSample.Resources;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationSample.API.Controllers
+{
+    [Route("{culture:culture}/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IStringLocalizer<SharedResource> localizer;
+        public HomeController(IStringLocalizer<SharedResource> localizer)
+        {
+            this.localizer = localizer;
+        }
+        public string Get()
+        {
+            return localizer["Home"];
+        }
+    }
+}
